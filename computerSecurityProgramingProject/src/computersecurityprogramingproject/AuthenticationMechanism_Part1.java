95ea0d26930d034f050a170a306ceb76048d1d9c
Smashy12 <cashmore12@gmail.com>
2017-11-18 16:39:55 -0500

    added periods

--- computerSecurityProgramingProject/src/computersecurityprogramingproject/AuthenticationMechanism_Part1.java
+++ computerSecurityProgramingProject/src/computersecurityprogramingproject/AuthenticationMechanism_Part1.java
@@ -2,7 +2,7 @@ package computersecurityprogramingproject;
 
 /**
  *
- * @author Ryan Richards, Chris Ashmore
+ * @author Ryan Richards, Chris Ashmore...
  */
 public class AuthenticationMechanism_Part1 {
 
@@ -18,7 +18,7 @@ public class AuthenticationMechanism_Part1 {
 /*
 Part 1: Implementation of password authentication mechanism
 In the first part, you need to implement a password authentication mechanism. You will create two
-programs. The first program registers and adds a new user into the system and stores the user’s password
+programs. The first program registers and adds a new user into the system and stores the users password
 information in a file. The second program is a user login program which asks for the username and
 password from the user and verifies it based on the information stored in the password file.
 
@@ -27,11 +27,11 @@ passwords. However, instead of storing the passwords in plain texts, the passwor
 message digests (hash) of the passwords to prevent attacks. You may use MD5 message digest scheme to
 create the message digest (hash) of the password.
 
-For example, if Tom and Harry are two existing users and if their passwords are “authentic” and
-“prevention”, then the password file will contain:
-where “973d98ac221d7e433fd7c417aa41027a” is the MD5 message digest of the password string
-“authentic”. Similarly, “7fd9a35dfa69c58a7ef0ecb4d53a1651” is the MD5 message digest of the
-password string “prevention”. A java code snippet for obtaining MD5 message digest is provided at the
+For example, if Tom and Harry are two existing users and if their passwords are authentic and
+prevention, then the password file will contain:
+where 973d98ac221d7e433fd7c417aa41027 is the MD5 message digest of the password string
+authentic. Similarly, 7fd9a35dfa69c58a7ef0ecb4d53a1651 is the MD5 message digest of the
+password string prevention. A java code snippet for obtaining MD5 message digest is provided at the
 end of this project description in Appendix 1 for your reference.
 
 Your first program should be able to register a new user by accepting a new username and password
