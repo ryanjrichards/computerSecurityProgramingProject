b33a86252dd0c0ab27e17e434e8667c56d50874a
Ryan Richards <ryan.richards95@gmail.com>
2017-11-18 16:34:16 -0500

    Added static void main

--- computerSecurityProgramingProject/src/computersecurityprogramingproject/PasswordCracker_Part2.java
+++ computerSecurityProgramingProject/src/computersecurityprogramingproject/PasswordCracker_Part2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package computersecurityprogramingproject;
 
 /**
@@ -11,6 +6,12 @@ package computersecurityprogramingproject;
  */
 public class PasswordCracker_Part2 {
     
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
 }
 
 /*
